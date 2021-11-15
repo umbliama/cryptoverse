1183fe58e07cb549df83612f58090621cdcdd5ec
Maxim Umblia <umblia34@gmail.com>
2021-11-15 11:43:21 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="body">
+    <header class="header"></header>
+    <section class="model">
+      <model-gltf
+        :backgroundColor="'#000'"
+        :backgroundAlpha="1"
+        src="models/glb/LP23.glb"
+      ></model-gltf>
+    </section>
+  </div>
+</template>
+
+
+<style scoped>
+.header {
+  background: url("~/static/header-bg.png") no-repeat;
+  background-position: 100% 100%;
+  height: 100vh;
+  background-size: cover;
+}
+</style>
+
+<script>
+import { ModelGltf } from "vue-3d-model";
+
+export default {
+  components: {
+    "model-gltf": ModelGltf,
+  },
+};
+</script>
